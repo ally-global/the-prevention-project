f4c7cccf87f5181d1ee874fdc677f84808ef781a
charcarr <carriero.char@gmail.com>
2024-03-02 16:19:46 +0100

    add grades 3-5 chapter 1 and 2

--- content/chapters.tsx
+++ content/chapters.tsx
@@ -25,19 +25,15 @@ export const chapters3to5: Chapter[] = [
           "https://vimeo.com/user112092565/download/908739053/339102d766",
         description: (
           <>
-            Hey there! Welcome to The Prevention Project, where we&apos;re about
-            to embark on an exciting journey together. In this special space
-            designed just for you, we&apos;re going to dive into some super
-            important topics that will help you navigate friendships and
-            relationships - both in person and online. Get ready for a fun and
-            interactive learning experience!
-            <br />
-            <br />
-            If anything ever feels confusing or if you have questions, reach out
-            to a safe adult in your life, or check out some of the resources at
-            the bottom of this page. Let&apos;s team up and make a real
-            difference in creating a safer, happier online and offline world for
-            everyone. Are you ready to jump in?
+            Welcome to The Prevention Project! Together, we&apos;re going to
+            dive into some super important topics that will help you navigate
+            friendships and relationships—both in person and online. Get ready
+            for a fun and interactive learning experience! If anything ever
+            feels confusing or if you have questions, reach out to a safe adult
+            in your life, or check out some of the resources at the bottom of
+            this page. Let&apos;s team up and make a real difference in creating
+            a safer, happier online and offline world for everyone. Are you
+            ready to jump in?
             <br />
             <br />
             Click a chapter below to get started!
@@ -55,6 +51,8 @@ export const chapters3to5: Chapter[] = [
         vimeoId: "908739159",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/908739159/d492647a2e",
+        facilitationNote:
+          "Facilitation Tip: Begin by sparking curiosity about consent and boundaries with an interactive warm-up activity. Have students draw or write down examples of situations where they've had to give consent in their daily lives on sticky notes. Then, create a consent wall where they can stick their notes and share their experiences with the group. This sets the stage for a lively discussion about the importance of consent and setting boundaries.",
         description: (
           <>
             In this first chapter, we&apos;re diving into a topic called Consent
@@ -71,6 +69,7 @@ export const chapters3to5: Chapter[] = [
         vimeoId: "916843254",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/916843254/a850328f68",
+        facilitationNote: `Facilitation Tip: After watching the video, invite the students to create their own "boundary bracelets" using colorful beads or string. As they design their bracelets, encourage them to think about what boundaries mean to them and what makes them feel comfortable or uncomfortable. Facilitate a sharing circle where they can explain the significance of their bracelet designs and discuss how setting boundaries empowers them in their everyday lives. Emphasize the importance of enthusiastic consent and how setting boundaries protects their ability to give consent.`,
         description: (
           <>
             Consent means giving permission with an enthusiastic YES! It&apos;s
@@ -81,7 +80,7 @@ export const chapters3to5: Chapter[] = [
             Whether it&apos;s deciding who to play with or asking for a hug, we
             navigate a world of consent in lots of ways. But here&apos;s the
             deal: true consent is given with enthusiasm, not just to be polite.
-            Discover the power of setting boundaries - like an invisible fence
+            Discover the power of setting boundaries – like an invisible fence
             around you - to protect your ability to consent, both in person and
             online.
           </>
@@ -92,6 +91,7 @@ export const chapters3to5: Chapter[] = [
         vimeoId: "908740111",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/908740111/aded50743c",
+        facilitationNote: `Facilitation Tip: Before starting the video, distribute true/false paddles or red and green flags to each participant. Instruct them to hold up the corresponding paddle or flag to indicate whether they believe each statement presented in the video is true or false. Pause the video after each statement to discuss their answers and reasoning as a group. Encourage and teamwork as they navigate through the dos and don'ts of consent and healthy boundaries.`,
         description: (
           <>
             Ever wondered about the subtle signals and body language that convey
@@ -99,8 +99,8 @@ export const chapters3to5: Chapter[] = [
             answering questions about sharing, hugs, and hanging out after
             school. Discover why understanding consent goes beyond just the
             spoken word, and why enthusiastic agreement is key. Ready for some
-            interactive learning? Click play and let the True and False
-            adventure begin!
+            interactive learning? Click play and let the True or False adventure
+            begin!
           </>
         ),
       },
@@ -109,18 +109,18 @@ export const chapters3to5: Chapter[] = [
         vimeoId: "916102481",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/916102481/8f5ba2ac5e",
+        facilitationNote:
+          "Facilitation Tip: Pause the video after each scenario and ask students to imagine themselves in the same situation. Prompt them to consider how they would respond and whether they would feel comfortable with the interactions shown. Encourage open discussion about the importance of clear communication and enthusiastic consent in everyday interactions.",
         description: (
           <>
             In this video, we witness everyday scenarios where kids like Priya,
             Jay, and Sam navigate giving and receiving consent. Whether
             it&apos;s borrowing a pen, playing catch, or sharing snacks,
             discover how these interactions embody the essence of enthusiastic
-            yes — where both minds and bodies are in sync.
-            <br />
-            <br />
-            As you&apos;re watching, feel free to pause and think about how you
-            might react in these situations? How would you respond? How would
-            you know if you&apos;re giving an enthusiastic yes?
+            yes – where both minds and bodies are in sync. As you&apos;re
+            watching, feel free to pause and think about how you might react in
+            these situations? How would you respond? How would you know if
+            you&apos;re giving an enthusiastic yes?
           </>
         ),
       },
@@ -128,11 +128,13 @@ export const chapters3to5: Chapter[] = [
         title: "Knowledge Snapshot",
         pdfUrl:
           "pdfs/grades3-5/Gr3-5_Ch1_Part5_Consent-and-Healthy-Boundaries.pdf",
+        facilitationNote:
+          "Facilitation Tip: Provide students with the activity page along with additional materials such as stickers, glitter pens, and colorful markers to enhance their creativity. Encourage them to personalize their activity page with drawings, collages, or written reflections about consent and healthy boundaries. As they work on their creations, facilitate discussions about the importance of setting boundaries and respecting each other's autonomy. Celebrate their unique ideas and contributions, fostering a sense of empowerment and self-expression.",
         description: (
           <>
             Now, it&apos;s your turn! Download or print this activity page to
             fill in your personal boundaries. What are the limits that make you
-            feel secure? Share your thoughts on consent — the freedom to agree
+            feel secure? Share your thoughts on consent - the freedom to agree
             or disagree without pressure. Discover the power of your
             enthusiastic &quot;yes&quot; and explore ways to communicate when
             someone isn&apos;t respecting your boundaries.
@@ -140,8 +142,7 @@ export const chapters3to5: Chapter[] = [
             <br />
             Ready for a creative challenge? Unleash your artistic side with the
             drawing prompt: &quot;Can you draw what consent means to you?&quot;
-            Click the link to download or print the activity page and make it
-            your own.
+            Download or print the activity page and make it your own!
             <br />
             <br />
             Congratulations! You finished the first Chapter on Consent & Healthy
@@ -159,28 +160,99 @@ export const chapters3to5: Chapter[] = [
         vimeoId: "908739333",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/908739333/9922cffe50",
+        facilitationNote: `Facilitation Tip: Start the chapter with an engaging icebreaker activity to get the students excited about exploring the internet. Have them create "digital passports" using colorful paper and stickers, where they write down their favorite online activities and interests. Encourage them to share their passports with the group and discuss what makes the internet so fascinating for them.`,
+        description: (
+          <>
+            Welcome to Chapter 2: The Internet! Ever wondered what makes the
+            online world so fascinating? We asked some friends what they love to
+            do online, and their answers were awesome. But here&apos;s the
+            thing: with 5 billion people connecting online, there&apos;s a lot
+            to explore and discover. Join us as we delve into the internet,
+            understanding who&apos;s out there, how people use it, and answering
+            some important questions about this vast digital space.
+          </>
+        ),
       },
       {
         title: "Say it Simple",
         vimeoId: "916102553",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/916102553/b95ee86896",
+        facilitationNote:
+          "Facilitation Tip: After watching the video, discuss with the students the various ways they use the internet and what they find exciting about it. Guide them in understanding that while the internet is a powerful tool, it's essential to use it responsibly. Encourage them to share their experiences and brainstorm positive and healthy ways to navigate the digital space.",
+        description: (
+          <>
+            Having access to the internet is like having the world in the palm
+            of your hand. We use it for gaming, shopping, homework, chatting
+            with friends, sharing photos, and watching videos. The internet can
+            connect us with over 5 billion people from around the world.
+            However, just because you have this powerful tool doesn&apos;t mean
+            you&apos;re always ready to use it. Let&apos;s explore how to
+            navigate this digital space positively and healthily.
+          </>
+        ),
       },
       {
         title: "True or False",
         vimeoId: "908740508",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/908740508/d600e19458",
+        facilitationNote: `Facilitation Tip: Pause the video at different points to discuss the scenarios presented and ask students how they would respond in similar situations. Emphasize the importance of online safety and the role of trusted adults in helping them navigate uncomfortable situations. Encourage open dialogue about setting boundaries and seeking help when needed.`,
+        description: (
+          <>
+            Welcome to &quot;That&apos;s not okay&quot; - the game where we
+            determine how to respond to content we find on the internet. Join us
+            as we answer questions about online safety, sharing insights on
+            gaming, and exploring the right actions to take when encountering
+            uncomfortable situations. Learn why it&apos;s crucial to involve
+            trusted adults in your online experiences. Ready to play? Click to
+            start!
+          </>
+        ),
       },
       {
         title: "Imagine This",
         vimeoId: "916102573",
         vimeoDownloadUrl:
           "https://vimeo.com/user112092565/download/916102573/8dddca4f3f",
+        facilitationNote: `Facilitation Tip: Pause the video after watching Jaime navigate the online situation and ask students to share their thoughts on what Jaime did well and what could have been done differently. Prompt them to identify safe adults in their lives whom they can turn to for help when faced with similar challenges online. Encourage empathy and critical thinking about online interactions.`,
+        description: (
+          <>
+            Boundaries can help us be safer and enjoy the world, even online. In
+            this video, we see Jaime navigating a tricky situation online. It
+            shows how important having safe adults is to help make sure that we
+            enjoy what we see and who we connect with. The internet has over 5
+            billion users from all around the world, and whether you&apos;re
+            playing games or chatting with friends, let&apos;s figure out how to
+            stay safe and still have a good time.
+            <br />
+            <br />
+            While you&apos;re watching, take a moment and think about what you
+            would do if you were in Jaime&apos;s shoes. Who are the safe adults
+            in your life who you can go to for help?
+          </>
+        ),
       },
       {
         title: "Knowledge Snapshot",
         pdfUrl: "pdfs/grades3-5/Gr3-5_Ch2_Part5_The-Internet.pdf",
+        facilitationNote: `Facilitation Tip: Provide students with the activity page along with additional materials such as stickers, glitter pens, and colorful markers to enhance their creativity and give them time to reflect on their online experiences. Encourage them to list activities that make them feel happy and safe online and to discuss the rules they have for internet use with their parents or guardians. After completing the activity, invite them to share their thoughts and drawings with the group, fostering creativity and discussion about online safety.`,
+        description: (
+          <>
+            Now, it&apos;s your turn to reflect on your online experiences!
+            Download or print this activity page to jot down all the things you
+            enjoy doing online that make you feel happy and safe. What about the
+            rules you and your parents have for using the internet safely? Write
+            those down too. Think about your online world, jot down your
+            thoughts, and keep those trusted adults in mind - they&apos;re your
+            go-to when you need guidance. Download or print the activity page
+            and add make it your own!
+            <br />
+            <br />
+            Well done! You&apos;ve completed Chapter 2 on The Internet. Next up,
+            let&apos;s talk about sharing information online.
+          </>
+        ),
       },
     ],
   },
