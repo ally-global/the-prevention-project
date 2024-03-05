a640f019053e69478c8b4af41b8b0c53f71b7fee
charcarr <carriero.char@gmail.com>
2024-03-05 20:42:54 +0100

    only show pdf on md or larger screens

--- components/PdfViewer.tsx
+++ components/PdfViewer.tsx
@@ -4,7 +4,7 @@ const PdfViewer = ({ pathToFile }: { pathToFile: string }) => {
   return (
     <Box
       sx={{
-        display: { xs: "none", sm: "flex" },
+        display: { xs: "none", sm: "none", md: "flex" },
         justifyContent: "center",
         alignItems: "center",
       }}
