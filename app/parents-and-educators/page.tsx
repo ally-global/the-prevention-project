350faaa4a2657e69fdea2405582969f4162280d1
devanpellow <devanpellow@gmail.com>
2024-08-25 21:12:25 -0400

    how to browse section

--- app/parents-and-educators/page.tsx
+++ app/parents-and-educators/page.tsx
@@ -1,25 +1,31 @@
-import HelpRightNowSection from "@/sections/HelpRightNowSection";
-import GenericHero from "@/sections/Heros/GenericHero";
-import LibrarySection, { Section } from "@/sections/LibrarySection";
-import WhatDoIDoSection from "@/sections/WhatDoIDoSection";
-import { Stack } from "@mui/material";
+import HelpRightNowSection from '@/sections/HelpRightNowSection';
+import ParentsAndEducatorsHero from '@/sections/Heros/ParentsAndEducatorsHero';
+import LibrarySection, { Section } from '@/sections/LibrarySection';
+import HowToBrowseSection from '@/sections/ParentsAndEducators/HowToBrowse';
+import WhatDoIDoSection from '@/sections/WhatDoIDoSection';
+import { Stack } from '@mui/material';
 
 export default function Library() {
-  return (
-    <>
-      <GenericHero text="Quickly find the topic you're looking for." />
-      <LibrarySection section={Section.THREE_TO_FIVE} />
-      <LibrarySection section={Section.SIX_TO_SEVEN} />
-      <LibrarySection section={Section.EIGHT_TO_TWELVE} />
-      <Stack
-        spacing={9}
-        sx={{
-          backgroundImage: "linear-gradient(to bottom right, #673BDC, #00B8C5)",
-        }}
-      >
-        <WhatDoIDoSection overflow />
-        <HelpRightNowSection />
-      </Stack>
-    </>
-  );
+	return (
+		<>
+			<ParentsAndEducatorsHero
+				title="For Parents and Educators"
+				text="Find the tools you need to facilitate conversations about safety, consent and exploitation prevention."
+				subtext="Whether you're guiding young minds in the classroom or supporting your child’s education at home, The Prevention Project has all the materials you need to deliver this vital education with confidence."
+			/>
+			<HowToBrowseSection />
+			<LibrarySection section={Section.THREE_TO_FIVE} />
+			<LibrarySection section={Section.SIX_TO_SEVEN} />
+			<LibrarySection section={Section.EIGHT_TO_TWELVE} />
+			<Stack
+				spacing={9}
+				sx={{
+					backgroundImage: 'linear-gradient(to bottom right, #673BDC, #00B8C5)',
+				}}
+			>
+				<WhatDoIDoSection overflow />
+				<HelpRightNowSection />
+			</Stack>
+		</>
+	);
 }
