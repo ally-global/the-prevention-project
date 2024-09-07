1a1ee0ce445b2875c9e11b2cfa5937b707720f45
devanpellow <devanpellow@gmail.com>
2024-09-07 20:22:35 +0200

    quotes

--- sections/ParentsAndEducators/LibraryTitle.tsx
+++ sections/ParentsAndEducators/LibraryTitle.tsx
@@ -1,15 +1,18 @@
-import StandardLayout from '@/components/StandardLayout';
-import Text from '@/components/Text';
+import StandardLayout from "@/components/StandardLayout";
+import Text from "@/components/Text";
 
 const LibraryTitle = () => {
 	return (
 		<StandardLayout
 			sx={{
-				backgroundColor: 'PrimaryBlue',
+				backgroundColor: "PrimaryBlue",
 				padding: { xs: 4, md: 6 },
 			}}
 		>
-			<Text variant="h3" sx={{ textAlign: 'center', fontSize: {xs: '36px', md:'44px'} }}>
+			<Text
+				variant="h3"
+				sx={{ textAlign: "center", fontSize: { xs: "36px", md: "44px" } }}
+			>
 				The Prevention Project Library
 			</Text>
 		</StandardLayout>
--- sections/ParentsAndEducators/Questions.tsx
+++ sections/ParentsAndEducators/Questions.tsx
@@ -1,33 +1,33 @@
-import StandardLayout from '@/components/StandardLayout';
-import Text from '@/components/Text';
-import { Stack, Link } from '@mui/material';
+import StandardLayout from "@/components/StandardLayout";
+import Text from "@/components/Text";
+import { Stack, Link } from "@mui/material";
 
 const QuestionsSection = () => {
 	return (
-		<StandardLayout sx={{ py: 6, marginX: 'auto' }}>
+		<StandardLayout sx={{ py: 6, marginX: "auto" }}>
 			<Stack
 				direction="column"
 				justifyContent="center"
 				alignItems="center"
 				spacing={2}
-				maxWidth={{ md: '497px' }}
+				maxWidth={{ md: "497px" }}
 			>
-				<Text variant={'h4'} color="PrimaryPurple" sx={{ mb: 2 }}>
+				<Text variant={"h4"} color="PrimaryPurple" sx={{ mb: 2 }}>
 					Questions?
 				</Text>
-				<Text variant={'h6'} color="PrimaryPurple" sx={{ fontSize: '20px' }}>
+				<Text variant={"h6"} color="PrimaryPurple" sx={{ fontSize: "20px" }}>
 					We&apos;re here to help.
 				</Text>
-				<Text variant={'body1'} color={'Grey900'} sx={{ textAlign: 'center' }}>
-					If you need further support, reach out to us by emailing{' '}
+				<Text variant={"body1"} color={"Grey900"} sx={{ textAlign: "center" }}>
+					If you need further support, reach out to us by emailing{" "}
 					<Link
 						href="mailto:thepreventionproject@ally.org"
-						sx={{ color: 'PrimaryPurple' }}
+						sx={{ color: "PrimaryPurple" }}
 					>
 						thepreventionproject@ally.org
-					</Link>{' '}
-					We&apos;re committed to ensuring you feel equipped and empowered to use The
-					Prevention Project effectively.
+					</Link>{" "}
+					We&apos;re committed to ensuring you feel equipped and empowered to
+					use The Prevention Project effectively.
 				</Text>
 			</Stack>
 		</StandardLayout>
